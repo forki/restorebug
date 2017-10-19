6089cc6934b9c8cd436399c31836973ef5d5a5e3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-19 17:40:06 +0200

    Targets error

---
+++ console/console.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projectdeps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
